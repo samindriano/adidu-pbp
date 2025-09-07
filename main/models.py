3e9eccae877e101f0ecfb48710b90989266945ef
samindriano <samuelindriano@gmail.com>
2025-09-07 21:29:54 +0700

    Progress 7/9/25

--- main/models.py
+++ main/models.py
@@ -1,3 +1,21 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    CATEGORY_CHOICES = [("sepatu lari", "Sepatu Lari"),
+                        ("sepatu bola", "Sepatu Bola"),
+                        ("kaus kaki", "Kaus Kaki"),
+                        ("jersey bola", "Jersey Bola")]
+
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    description = models.TextField()
+    thumbnail = models.URLField(blank=True, null=True)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default="sepatu lari")
+    is_featured = models.BooleanField(default=False)
+    brand = models.CharField(max_length=255)
+    stock = models.IntegerField()
+
+
+    def __str__(self):
+        return self.name
