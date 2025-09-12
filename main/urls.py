4534d9fdc73baf93187125ef88560125cc5086ab
samindriano <samuelindriano@gmail.com>
2025-09-13 01:59:33 +0700

    test tugas 3

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, show_json, show_xml, show_xml_by_id, show_json_by_id
+from main.views import show_main, show_json, show_xml, show_xml_by_id, show_json_by_id, create_product, show_product
 
 app_name = 'main'
 
@@ -7,6 +7,8 @@ urlpatterns = [
     path('', show_main, name='show_main'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
-    path('xml/<str:news_id>/', show_xml_by_id, name='show_xml_by_id'),
-    path('json/<str:news_id>/', show_json_by_id, name='show_json_by_id'),
+    path('xml/<str:product_id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<str:product_id>/', show_json_by_id, name='show_json_by_id'),
+    path('create-product/', create_product, name='create_product'),
+    path('news/<str:id>/', show_product, name='show_product'),
 ]
