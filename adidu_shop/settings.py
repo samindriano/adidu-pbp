16d6640fcab1edb2bcd6aacc9bc8cb66cdde7adf
samindriano <samuelindriano@gmail.com>
2025-09-13 13:01:54 +0700

    test tugas 3

--- adidu_shop/settings.py
+++ adidu_shop/settings.py
@@ -33,6 +33,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "samuel-indriano-adidushop.pbp.cs.ui.ac.id"]
 CSRF_TRUSTED_ORIGINS = [
     "https://samuel-indriano-adidushop.pbp.cs.ui.ac.id/",
+    "https://pbp.cs.ui.ac.id",
     "https://*.pbp.cs.ui.ac.id",
 ]
 
